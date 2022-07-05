7c66c7061f82d98e88214e949f82b43f0bbcf3ab
GalyautdinovIslam <galyautdinovislam@gmail.com>
2022-07-05 16:37:34 +0300

    Spring Boot initialization

---
+++ src/main/java/ru/itis/og/OgApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.og;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OgApplication.class, args);
+    }
+
+}
